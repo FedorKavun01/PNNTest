ec82dbdcf13cea549dd0a5b0b4cfa52d0e858946
Unknown <omax1972@gmail.com>
2020-09-07 09:35:07 +0300

    Create Film Activity

---
+++ app/src/main/java/com/example/pnntest/View/FilmActivity.kt
@@ -0,0 +1,14 @@
+
+package com.example.pnntest.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.pnntest.R
+
+class FilmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film)
+    }
+}
